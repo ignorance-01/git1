e8219fba68f4374b478de19b6a8e5f0420101a97
ignorance <1357684753@qq.com>
2021-09-10 16:04:29 +0800

    first

---
+++ src/main/java/com/atguigu/git/GitDemo.java
@@ -0,0 +1,14 @@
+package com.atguigu.git;
+
+/**
+ * @author ：陈海江
+ * @date ：Created in 2021/9/10 16:03
+ * @description：gitdemo
+ * @modified By：
+ * @version: 1.0$
+ */
+public class GitDemo {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
